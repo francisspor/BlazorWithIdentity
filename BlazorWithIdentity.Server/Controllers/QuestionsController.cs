3c0a000fedd2c8ee8af3550c780fcde2ed05833f
Francis Spor <francis@upstatespors.com>
2019-08-20 21:26:57 -0400

    More files

---
+++ BlazorWithIdentity.Server/Controllers/QuestionsController.cs
@@ -0,0 +1,34 @@
+﻿using BlazorWithIdentity.Shared;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace BlazorWithIdentity.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [Authorize]
+    public class QuestionsController
+    {
+        [HttpGet("[action]")]
+        public IEnumerable<Question> GetQuestions()
+        {
+            return new List<Question> {
+                new Question
+                {
+                    Text = "What color is the sky?",
+                    Description = "A common question from young children.",
+                    QuestionType = QuestionType.Text,
+                    Required = false
+                },
+                new Question
+                {
+                    Text = "What's the answer to life, the universe and everything?",
+                    Description = "A question for Deep Thought.",
+                    QuestionType = QuestionType.Text,
+                    Required = true
+                },
+
+            };
+        }
+    }
+}
---
+++ BlazorWithIdentity.Shared/Question.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Humanizer;
+
+namespace BlazorWithIdentity.Shared
+{
+    public class Question
+    {
+        public string QuestionId => $"{Text.Dehumanize()}_Question";
+
+        public bool Required { get; set; }
+
+        public string Text { get; set; }
+
+        public string Description { get; set; }
+
+        public QuestionType QuestionType { get; set; }
+
+        public int? MaxLength { get; set; }
+
+        public List<PossibleAnswer> PossibleAnswers { get; set; }
+
+        public Answer Answer { get; set; }
+    }
+
+    public class Answer
+    {
+        public string Value { get; set; }
+    }
+
+    public class PossibleAnswer
+    {
+        public int Value { get; set; }
+
+        public string Text { get; set; }
+    }
+
+    public enum QuestionType
+    {
+        Text,
+        Number,
+        List,
+        YesNo,
+        TrueFalse
+    }
+}
